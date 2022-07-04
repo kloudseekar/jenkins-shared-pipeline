98064d6a8036faa8146bc3ad471144059edfdd1a
Maninder <kloudseekar@gmail.com>
2022-07-04 12:28:22 +0530

    Initial Commit for Jenkins Terrform shared library

---
+++ vars/terraformInit.groovy
@@ -0,0 +1,20 @@
+def terraformInit() {
+    dir("${env.WORKSPACE}/codebase/us-east-1-eks"){
+            sh("""
+          echo ""
+          echo "*************** TERRAFOM INIT ******************"
+          echo "******* At environment: ${params.terraform_workspace} ********"
+          echo "*************************************************"
+            terraform init -backend-config=config.s3.tfbackend
+            echo '\033[Hello colorful world!\033'
+            """)
+                    script {
+                        try {
+                            sh "terraform workspace new ${params.terraform_workspace}"
+                        } catch (err) {
+                            sh "terraform workspace select ${params.terraform_workspace}"
+                        }
+                    }
+        }
+
+}
