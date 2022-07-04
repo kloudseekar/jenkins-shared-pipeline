56747b7c6bb4d507e8d67fbc32682ac8fe6e2458
Maninder <kloudseekar@gmail.com>
2022-07-04 18:07:00 +0530

    adding apply function

---
+++ vars/terraformApply.groovy
@@ -0,0 +1,16 @@
+
+def call(String action){
+                dir("${env.WORKSPACE}/codebase/us-east-1-eks"){
+
+                    if(params.Action == 'Destroy') {
+                        env.DESTROY = '-destroy'
+                    } else {
+                        env.DESTROY = ""
+                    }
+
+                        unstash "terraform-plan"
+                        sh "terraform apply ${env.DESTROY} terraform.tfplan -lock=false;echo \$? > status"
+
+                    }
+                }
+
