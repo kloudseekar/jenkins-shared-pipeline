192a01a650fc93949a32235da4d3caee50ab3cfa
Maninder <kloudseekar@gmail.com>
2022-07-19 00:34:42 +0530

    with map config

--- vars/terraformPlan.groovy
+++ vars/terraformPlan.groovy
@@ -21,7 +21,7 @@ def call(Map conf){
                     } else {
                         env.DESTROY = ""
                     }
-                        sh "terraform plan ${env.DESTROY} -out terraform.tfplan -lock=false -var instance_type=${conf.instance_type}  -var asg_desired_capacity=${conf.asg_desired_capacity}"
+                        sh "terraform plan ${env.DESTROY} -out terraform.tfplan -lock=false -var instance_type=${conf.instance_type}  -var asg_desired_capacity=${conf.desired_number}"
                         stash name: "terraform-plan", includes: "terraform.tfplan"
                     }
                 }
