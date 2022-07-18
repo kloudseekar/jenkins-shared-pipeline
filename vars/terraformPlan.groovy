eacf50d83957b48e98a1f532d743242126e4bae3
Maninder <kloudseekar@gmail.com>
2022-07-19 00:20:35 +0530

    with map config

--- vars/terraformPlan.groovy
+++ vars/terraformPlan.groovy
@@ -1,14 +1,27 @@
 
-def call(String action , String instance_type , String asg_desired_capacity){
-                dir("${env.WORKSPACE}/codebase/us-east-1-eks"){
+// def call(String action , String instance_type , String asg_desired_capacity){
+//                 dir("${env.WORKSPACE}/codebase/us-east-1-eks"){
+
+//                     if(action == 'destroy') {
+//                         env.DESTROY = '-destroy'
+//                     } else {
+//                         env.DESTROY = ""
+//                     }
+//                         sh "terraform plan ${env.DESTROY} -out terraform.tfplan -lock=false -var instance_type=${instance_type}  -var asg_desired_capacity=${asg_desired_capacity}"
+//                         stash name: "terraform-plan", includes: "terraform.tfplan"
+//                     }
+//                 }
+
+
+def call(Map conf){
+                dir("${env.WORKSPACE}/${conf.path}"){
 
                     if(action == 'destroy') {
                         env.DESTROY = '-destroy'
                     } else {
                         env.DESTROY = ""
                     }
-                        sh "terraform plan ${env.DESTROY} -out terraform.tfplan -lock=false -var instance_type=${instance_type}  -var asg_desired_capacity=${asg_desired_capacity}"
+                        sh "terraform plan ${env.DESTROY} -out terraform.tfplan -lock=false -var instance_type=${conf.instance_type}  -var asg_desired_capacity=${conf.asg_desired_capacity}"
                         stash name: "terraform-plan", includes: "terraform.tfplan"
                     }
                 }
-
