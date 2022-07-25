ff8e75278e268600c0970a4f39e20c6335cbaa35
Maninder <kloudseekar@gmail.com>
2022-07-25 10:16:00 +0530

    Initial commit with service deploy template

---
+++ vars/serviceDeployNonProd.groovy
@@ -0,0 +1,101 @@
+def call(Map conf) {
+
+properties([
+    parameters([
+        [$class: 'ChoiceParameter',
+            choiceType: 'PT_SINGLE_SELECT',
+            description: 'Select a Region',
+            filterLength: 1,
+            filterable: false,
+            name: 'AWS_REGION',
+            randomName: 'choice-parameter-7601235200970',
+            script: [$class: 'GroovyScript',
+                fallbackScript: [classpath: [], sandbox: true, script: 'return ["ERROR"]'],
+                script: [classpath: [], sandbox: true, script: 'return ["us-east-1","us-east-2","ap-south-1"]']
+                ]],
+        [$class: 'ChoiceParameter',
+            choiceType: 'PT_SINGLE_SELECT',
+            description: 'Select Workspace',
+            filterLength: 1,
+            filterable: false,
+            name: 'terraform_workspace',
+            randomName: 'choice-parameter-7601235200970',
+            script: [$class: 'GroovyScript',
+                fallbackScript: [classpath: [], sandbox: true, script: 'return ["ERROR"]'],
+                script: [classpath: [], sandbox: true, script: 'return ["dev","qa","prod"]']
+                ]],
+        [$class: 'ChoiceParameter',
+            choiceType: 'PT_SINGLE_SELECT',
+            description: 'Terraform Operation',
+            filterLength: 1,
+            filterable: false,
+            name: 'operation',
+            randomName: 'choice-parameter-7601235200970',
+            script: [$class: 'GroovyScript',
+                fallbackScript: [classpath: [], sandbox: true, script: 'return ["ERROR"]'],
+                script: [classpath: [], sandbox: true, script: 'return ["init","plan","apply","destroy"]']
+                ]],
+
+        string (defaultValue: 't2.micro', name: 'instance_type', trim: true),
+        string(name: "BRANCH_NAME", defaultValue: "main", trim: true, description: "Git  Branch or  Tag to be dpeloyed")
+        
+    ])
+    
+        
+])
+
+
+pipeline {
+    agent {
+      label 'linux'
+    }
+    options {
+        ansiColor('xterm')
+    }
+    
+    environment {
+        AWS_ACCESS_KEY_ID     = credentials('EKS_AWS_ACCESS_KEY_ID')
+        AWS_SECRET_ACCESS_KEY = credentials('EKS_AWS_SECRET_ACCESS_KEY')
+        TF_IN_AUTOMATION      = 'true'
+    }
+    
+    stages{
+        stage('Git Checkout & Setup') {
+            steps{
+                checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: params.BRANCH_NAME]], extensions: [], userRemoteConfigs: [[url: conf.giturl]]]
+                script  {
+                    conf.aws_region=params.AWS_REGION
+                    conf.action=params.operation
+                    conf.instance_type=params.instance_type
+                    conf.desired_number=params.desired_number
+                    conf.branch_name= params.BRANCH_NAME
+                    println conf
+
+                }
+
+            }
+        }
+
+        stage('Dummy Step')
+        {
+            sh 'ls -ltr'
+        }
+
+
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+            
+        
