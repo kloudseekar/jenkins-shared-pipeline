390d5972db200256ec21d2e98cf991cae8a87eab
Maninder <kloudseekar@gmail.com>
2022-07-27 12:05:24 +0530

    going for full run

--- vars/serviceDeployNonProd.groovy
+++ vars/serviceDeployNonProd.groovy
@@ -27,61 +27,61 @@ def call(Map conf) {
         }
 
         stages {
-            // stage('Git Checkout & Setup') {
-            //     steps {
-            //         checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: params.BRANCH_NAME]], extensions: [], userRemoteConfigs: [[url: conf.giturl]]]
-            //         script  {
-            //             conf.aws_region = params.AWS_REGION
-            //             conf.action = params.operation
-            //             conf.instance_type = params.instance_type
-            //             conf.desired_number = params.desired_number
-            //             conf.branch_name = params.BRANCH_NAME
-            //             println conf
-            //             currentBuild.description = 'My custom build description'
-            //         }
-            //     }
-            // }
-            // stage('Build  Artifact') {
-            //     environment {
-            //         // SCANNER_HOME = tool 'SonarQubeScanner'
-            //         ORGANIZATION = 'ac-maninder'
-            //         PROJECT_NAME = 'petclinic'
-            //     }
-            //     steps {
-            //         withSonarQubeEnv('SonarCloud-AC') {
-            //             sh '''mvn clean package'''
-            //         }
-            //     }
-            // }
+            stage('Git Checkout & Setup') {
+                steps {
+                    checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: params.BRANCH_NAME]], extensions: [], userRemoteConfigs: [[url: conf.giturl]]]
+                    script  {
+                        conf.aws_region = params.AWS_REGION
+                        conf.action = params.operation
+                        conf.instance_type = params.instance_type
+                        conf.desired_number = params.desired_number
+                        conf.branch_name = params.BRANCH_NAME
+                        println conf
+                        currentBuild.description = 'My custom build description'
+                    }
+                }
+            }
+            stage('Build  Artifact') {
+                environment {
+                    // SCANNER_HOME = tool 'SonarQubeScanner'
+                    ORGANIZATION = 'ac-maninder'
+                    PROJECT_NAME = 'petclinic'
+                }
+                steps {
+                    withSonarQubeEnv('SonarCloud-AC') {
+                        sh '''mvn clean package'''
+                    }
+                }
+            }
 
-            // stage('SonarCloud') {
-            //     environment {
-            //         // SCANNER_HOME = tool 'SonarQubeScanner'
-            //         ORGANIZATION = 'ac-maninder'
-            //         PROJECT_NAME = 'petclinic-service'
-            //     }
-            //     steps {
-            //         withSonarQubeEnv('SonarCloud-AC') {
-            //             sh '''mvn sonar:sonar -Dsonar.organization=$ORGANIZATION \
-            //                       -Dsonar.java.binaries=target/classes/ \
-            //                       -Dsonar.projectKey=$PROJECT_NAME \
-            //                       -Dsonar.sources=. \
-            //                       -Dsonar.exclusions=src/test/java/****/*.java'''
-            //         }
-            //     }
-            // }
+            stage('SonarCloud') {
+                environment {
+                    // SCANNER_HOME = tool 'SonarQubeScanner'
+                    ORGANIZATION = 'ac-maninder'
+                    PROJECT_NAME = 'petclinic-service'
+                }
+                steps {
+                    withSonarQubeEnv('SonarCloud-AC') {
+                        sh '''mvn sonar:sonar -Dsonar.organization=$ORGANIZATION \
+                                  -Dsonar.java.binaries=target/classes/ \
+                                  -Dsonar.projectKey=$PROJECT_NAME \
+                                  -Dsonar.sources=. \
+                                  -Dsonar.exclusions=src/test/java/****/*.java'''
+                    }
+                }
+            }
 
-            // stage('Quality Gate') {
-            //     steps {
-            //         sleep(10)
-            //         /* groovylint-disable-next-line DuplicateNumberLiteral */
-            //         timeout(time: 10, unit: 'MINUTES') {
-            //             // Parameter indicates whether to set pipeline to UNSTABLE if Quality Gate fails
-            //             // true = set pipeline to UNSTABLE, false = don't
-            //             waitForQualityGate abortPipeline: true
-            //         }
-            //     }
-            // }
+            stage('Quality Gate') {
+                steps {
+                    sleep(10)
+                    /* groovylint-disable-next-line DuplicateNumberLiteral */
+                    timeout(time: 10, unit: 'MINUTES') {
+                        // Parameter indicates whether to set pipeline to UNSTABLE if Quality Gate fails
+                        // true = set pipeline to UNSTABLE, false = don't
+                        waitForQualityGate abortPipeline: true
+                    }
+                }
+            }
 
             stage('Build Image') {
                 parallel {
@@ -132,29 +132,7 @@ def call(Map conf) {
                     }
                 }
             }
-            // stage('Deploy') {
-            //     steps {
-            //         script {
-            //             /* groovylint-disable-next-line NestedBlockDepth */
-            //             docker.withRegistry('', dockerRegistryCred) {
-            //                 dockerImage.push("${env.BUILD_NUMBER}")
-            //                 dockerImage.push('latest')
-            //             }
-            //         }
-            //     }
-            // }
-            // stage('Deploy Jfrog Artifactory') {
-            //     steps {
-            //         script {
-            //             /* groovylint-disable-next-line NestedBlockDepth */
-            //             docker.withRegistry(artifactoryRegistryUrl, artifactoryRegistryCred) {
-            //                 dockerImageArt.push("${env.BUILD_NUMBER}")
-            //                 /* groovylint-disable-next-line DuplicateStringLiteral */
-            //                 dockerImageArt.push('jfrog')
-            //             }
-            //         }
-            //     }
-            // }
+           
                 stage('Remove Unused docker image') {
                 steps {
                     script {
@@ -165,15 +143,16 @@ def call(Map conf) {
                 }
                 }
         }
-    // post {
-    //     always {
-    //         steps {
-    //             script {
-    //             echo 'One way or another, I have finished'
-    //             deleteDir() /* clean up our workspace */
-    //             }
-    //         }
-    //     }
+    post {
+        always {
+            steps {
+                script {
+                echo 'One way or another, I have finished'
+                deleteDir() /* clean up our workspace */
+                }
+            }
+        }
+    }
     //     success {
     //         steps {
     //             script {
